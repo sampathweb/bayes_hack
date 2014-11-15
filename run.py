2de4cedaa758930e6187ec7eaf5ec044dd6ac522
Ramesh Sampath <ramesh@sampathweb.com>
2014-11-15 15:23:20 -0800

    update to port

--- run.py
+++ run.py
@@ -4,4 +4,4 @@ from app import create_app
 if __name__ == '__main__':
     env = 'hack'
     app = create_app('app.settings.%sConfig' % env.capitalize(), env)
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=8000, debug=True)
