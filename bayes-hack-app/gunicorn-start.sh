8b603929f0ff9ff5dcc7148a46b1b41924df1ff8
Ramesh Sampath <ramesh@sampathweb.com>
2014-11-15 18:08:17 -0800

    gunicorn

---
+++ bayes-hack-app/gunicorn-start.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+NAME="Donors Choose App"
+SOCKFILE=/home/ramesh/sock
+USER=ramesh
+GROUP=staff
+NUM_WORKERS=3
+
+echo "Starting $NAME"
+
+export PYTHONPATH=/usr/local/miniconda/bin
+
+# Create the run directory if it doesn't exist
+RUNDIR=$(dirname $SOCKFILE)
+test -d $RUNDIR || mkdir -p $RUNDIR
+
+# Start your unicorn
+exec gunicorn run:app -b 0.0.0.0:8000 \
+  --name $NAME \
+  --workers $NUM_WORKERS \
+  --user=$USER --group=$GROUP \
+  --log-level=debug \
+  --bind=unix:$SOCKFILE
