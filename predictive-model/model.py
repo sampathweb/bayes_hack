2289c5dd4913d77cac9aebe12d450b4771e74ec4
Greg Novak <greg.novak@gmail.com>
2014-11-22 14:19:52 -0800

    Rearrange code

--- predictive-model/model.py
+++ predictive-model/model.py
@@ -19,69 +19,10 @@ import numpy as np
 import pandas as pd
 import pylab as pl
 
-def can(obj, file, protocol=2):
-    """More convenient syntax for pickle, intended for interactive use
-
-    Most likely:
-    >>> can([1,2,3], 'file.dat')
-    But can also do:
-    >>> with open('file.dat', 'w') as f: can([1,2,3], f); can((3,4,5), f)
-
-    """
-    if type(file) is str: f=open(file,'wb')
-    else: f=file
-
-    cPickle.dump(obj, f, protocol=protocol)
-
-    if type(file) is str: f.close()
-
-def uncan(file):
-    """More convenient syntax for pickle, intended for interactive use
-
-    Most likely:
-    >>> obj = uncan('file.dat')
-    But can also do:
-    >>> with open('file.dat') as f: foo = uncan(f); bar = uncan(f)
-
-    """
-    # If filename, should this read until all exhausted?
-    if type(file) is str: f=open(file, 'rb')
-    else: f=file    
-
-    obj = cPickle.load(f)
-
-    if type(file) is str: f.close()
-
-    return obj
-
-def project_data(fn='opendata_projects.csv'):
-    return pd.read_csv(fn,
-                       parse_dates=['date_expiration','date_thank_you_packet_mailed',
-                                    'date_completed', 'date_posted'])
-    
-    
-def cat2numerical(feature,nan_column=False):
-    categories = set(feature)
-    if not nan_column:
-        categories = [cat for cat in categories if cat is not np.nan]
-    print len(categories), categories
-    new_features = []
-    for cat in categories:
-        col = pd.Series(np.zeros(len(feature)))
-        col[feature == cat] = 1
-        new_features.append(col)
-    return pd.DataFrame(new_features).T
-
-# decision tree for funded vs. not funded
-def discretize(col):
-    col = np.asarray(col)
-    vals = sorted(list(set(col)))
-    result = np.ones(len(col), 'i')
-    result_dict = {}
-    for idx, val in enumerate(vals):
-        result[col==val] = idx
-        result_dict[idx] = val
-    return result, result_dict
+##############################
+# Which features to include,
+# what are their types?
+##############################
 
 features = [
     # categorical, lots of categories
@@ -109,6 +50,102 @@ features = [
     # Exclude, giveaway
     # u'funding_status', u'date_posted', u'date_completed', u'date_thank_you_packet_mailed', u'date_expiration'
 ]
+
+# These have some number of 
+categorical_features = [
+    u'_projectid', u'_teacher_acctid', u'_schoolid', u'school_ncesid',
+    u'school_city', u'school_state', u'school_zip', u'school_metro',
+    u'school_district', u'school_county',
+    u'teacher_prefix', u'primary_focus_subject', u'primary_focus_area',
+    u'secondary_focus_subject', u'secondary_focus_area', u'resource_type',
+    u'poverty_level', u'grade_level',]
+
+# All of these are tf
+binary_features = [
+    u'school_charter', 
+    u'school_magnet',
+    u'school_year_round',
+    u'school_nlns',
+    u'school_kipp',
+    u'school_charter_ready_promise',
+    u'teacher_teach_for_america',
+    u'teacher_ny_teaching_fellow',
+    u'eligible_double_your_impact_match',
+    u'eligible_almost_home_match']
+
+##############################
+# I/O
+##############################
+
+def project_data(fn='opendata_projects.csv'):
+    return pd.read_csv(fn,
+                       parse_dates=['date_expiration','date_thank_you_packet_mailed',
+                                    'date_completed', 'date_posted'])
+        
+##############################
+# Construct full data sets
+##############################
+
+def make_data_set(zz, state='CA'):
+    ww = pre_screen(zz)
+    # important to featureize _without_ restricting to state so that categorization happens correctly
+    xx, enc, fwd, bkw = featureize(ww)
+    # now select by state
+    
+    yy_all = funded_or_not(zz)
+    # Pull out just the ones that are included 
+    yy = yy_all.loc[ww.index]
+    return xx.toarray(), yy.values.astype('i'), ww.index, enc, fwd, bkw
+
+def funded_or_not(zz):
+    return pd.isnull(zz.date_completed-zz.date_posted)
+
+def time_to_fund():
+    return pd.isnull(zz.date_completed-zz.date_posted)
+
+##############################
+# Model Fitting
+##############################
+
+def fit_model(xx,yy):
+    model = skl.ensemble.RandomForestRegressor()
+    model.fit(xx,yy)
+    return model
+
+def make_ca_model(zz):
+    xx, yy, idx, enc, fwd, bkw = make_data_set(zz, 'CA')
+    print xx.shape
+    model = skl.ensemble.RandomForestRegressor()
+    model.fit(xx,yy)
+    can(model, 'ca-model.pkl')
+    return model
+
+##############################
+# Featurization
+##############################
+
+def cat2numerical(feature,nan_column=False):
+    categories = set(feature)
+    if not nan_column:
+        categories = [cat for cat in categories if cat is not np.nan]
+    print len(categories), categories
+    new_features = []
+    for cat in categories:
+        col = pd.Series(np.zeros(len(feature)))
+        col[feature == cat] = 1
+        new_features.append(col)
+    return pd.DataFrame(new_features).T
+
+# decision tree for funded vs. not funded
+def discretize(col):
+    col = np.asarray(col)
+    vals = sorted(list(set(col)))
+    result = np.ones(len(col), 'i')
+    result_dict = {}
+    for idx, val in enumerate(vals):
+        result[col==val] = idx
+        result_dict[idx] = val
+    return result, result_dict
 
 def pre_screen(zz, state='CA'):
     by_state = zz[zz.school_state==state]
@@ -159,29 +196,6 @@ def featureize(zz):
             return non_cat_labels[offset], None
         return key, label
 
-    # These have some number of 
-    categorical_features = [
-        u'_projectid', u'_teacher_acctid', u'_schoolid', u'school_ncesid',
-        u'school_city', u'school_state', u'school_zip', u'school_metro',
-        u'school_district', u'school_county',
-        u'teacher_prefix', u'primary_focus_subject', u'primary_focus_area',
-        u'secondary_focus_subject', u'secondary_focus_area', u'resource_type',
-        u'poverty_level', u'grade_level',
-    ]
-
-    # All of these are tf
-    binary_features = [
-        u'school_charter', 
-        u'school_magnet',
-        u'school_year_round',
-        u'school_nlns',
-        u'school_kipp',
-        u'school_charter_ready_promise',
-        u'teacher_teach_for_america',
-        u'teacher_ny_teaching_fellow',
-        u'eligible_double_your_impact_match',
-        u'eligible_almost_home_match']
-
     #features = ['num', 'city', 'color', 'val']
     #categorical_features = ['city', 'color']
 
@@ -209,33 +223,42 @@ def featureize(zz):
         
     return xx, encode, forward_transform, reverse_transform
 
-def make_data_set(zz, state='CA'):
-    ww = pre_screen(zz)
-    # important to featureize _without_ restricting to state so that categorization happens correctly
-    xx, enc, fwd, bkw = featureize(ww)
-    # now select by state
-    
-    yy_all = funded_or_not(zz)
-    # Pull out just the ones that are included 
-    yy = yy_all.loc[ww.index]
-    return xx.toarray(), yy.values.astype('i'), ww.index, enc, fwd, bkw
+##############################
+# Utilities
+##############################
 
-def funded_or_not(zz):
-    return pd.isnull(zz.date_completed-zz.date_posted)
+def can(obj, file, protocol=2):
+    """More convenient syntax for pickle, intended for interactive use
 
-def time_to_fund():
-    return pd.isnull(zz.date_completed-zz.date_posted)
+    Most likely:
+    >>> can([1,2,3], 'file.dat')
+    But can also do:
+    >>> with open('file.dat', 'w') as f: can([1,2,3], f); can((3,4,5), f)
 
-def fit_model(xx,yy):
-    model = skl.ensemble.RandomForestRegressor()
-    model.fit(xx,yy)
-    return model
+    """
+    if type(file) is str: f=open(file,'wb')
+    else: f=file
 
-def make_ca_model(zz):
-    xx, yy, idx, enc, fwd, bkw = make_data_set(zz, 'CA')
-    print xx.shape
-    model = skl.ensemble.RandomForestRegressor()
-    model.fit(xx,yy)
-    can(model, 'ca-model.pkl')
-    return model
+    cPickle.dump(obj, f, protocol=protocol)
+
+    if type(file) is str: f.close()
+
+def uncan(file):
+    """More convenient syntax for pickle, intended for interactive use
+
+    Most likely:
+    >>> obj = uncan('file.dat')
+    But can also do:
+    >>> with open('file.dat') as f: foo = uncan(f); bar = uncan(f)
+
+    """
+    # If filename, should this read until all exhausted?
+    if type(file) is str: f=open(file, 'rb')
+    else: f=file    
+
+    obj = cPickle.load(f)
+
+    if type(file) is str: f.close()
+
+    return obj
 
